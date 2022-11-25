fb91b754e9deda826bf70ffea590f362eae3eb55
JC4F <lequan12122002@gmail.com>
2022-11-25 20:47:37 +0700

    handle when f5 page

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -36,11 +36,13 @@ const verifySSOToken = async(req, res) => {
 
             //set cookie
             res.cookie('access_token', token, {
-                maxAge: +process.env.MAX_AGE_ACCESS_TOKEN,
+                // maxAge: +process.env.MAX_AGE_ACCESS_TOKEN,
+                maxAge: 900*1000,
                 httpOnly: true
             });
             res.cookie('refresh_token', refreshToken, {
-                maxAge: +process.env.MAX_AGE_REFRESH_TOKEN,
+                // maxAge: +process.env.MAX_AGE_REFRESH_TOKEN,
+                maxAge: 3600*1000,
                 httpOnly: true
             });
 
